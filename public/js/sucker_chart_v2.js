68ffaed5858c5eb5bb03f363587fc43fac5a51b5
Lenny Martinez <lennymartinezd@gmail.com>
2019-07-20 19:38:33 -0400

    successfully add in the charts for all the sections

--- public/js/sucker_chart_v2.js
+++ public/js/sucker_chart_v2.js
@@ -4,12 +4,14 @@ class sucker_chart {
 		// these are the inputs: the data we're working with, and the element we're loading the chart into.
 		this.data = opts.plot_data;
 		this.element = opts.element;
-		this.col_x = opts.x;
-		this.col_y = opts.y;
-		this.rank = "rank_" + this.col_x;
+		this.x = opts.x;
+		// this.col_y = opts.y;
+		this.rank = "rank_" + this.x;
+		this.filter = opts.filter + 1;
+		this.ascending = opts.ascending;
 
-		this.normalize = 0;
-		this.sort(this.col_x);
+		this.normalize = false;
+		this.sort(this.x);
 	}
 
 	draw() {
@@ -17,7 +19,7 @@ class sucker_chart {
 			top: 50,
 			right: 60,
 			bottom: 50,
-			left: 100};
+			left: 150};
 
 		this.width = window.innerWidth - this.margin.left - this.margin.right;
 		this.height = window.innerHeight - this.margin.bottom - this.margin.top;
@@ -29,37 +31,37 @@ class sucker_chart {
 
 
 		this.svg = anchor.append("svg")
-					.attr("width", this.width + this.margin.left + this.margin.right)
-					.attr("height", this.height + this.margin.top + this.margin.bottom)
+					.attr("viewBox", `0 0 ${this.width + this.margin.left + this.margin.right} ${this.height + this.margin.top +this.margin.bottom}`)
+					.style("width", "100%")
+					.style("height", "auto")
+					.attr("class", "sucker")
 					.append('g')
 					.attr('transform', `translate(${this.margin.left},${this.margin.top})`);
 		
-		this.data = this.data.filter(d => d[this.rank] < 11);
-		this.create_scales(this.normalize);
+		this.data = this.data.filter(d => d[this.rank] < this.filter);
+		this.create_scales();
 		this.create_axes();
-		this.create_shapes(this.col_x, this.col_y);
+		this.create_shapes();
 	}
 
-	create_scales(normalize) {
-		//make the scales
-		const n = normalize;
-		const x_max = d3.max(this.data, (d) => { return d[this.col_x]; });
-		this.x_scale = d3.scaleLinear().range([0, this.width]);
-		if (n === 0) {
-			this.x_scale.domain([0, Math.floor(x_max * 1.2)]);
-		} else if (n === 1) {
-			this.x_scale.domain([0, 100]);
-		}
-		
-		this.y_scale2 = d3.scaleBand()
-						.domain(this.data.map((d) => { return d[this.col_y];}))
-						.range([0, this.height])
-						.padding(5);
-		
-		this.y_scale = d3.scaleLinear()
-					.domain([1949, 2020])
-					.range([0, this.height]);
+	create_scales() {
+		const x_max = d3.max(this.data, d => d[this.x]);
+		var k;
 
+		if (this.normalize) {
+			k = 100;
+		} else {
+			k = Math.floor(x_max * 1.1);
+		}
+
+		this.x_scale = d3.scaleLinear()
+						.domain([0, k])
+						.range([0, this.width]);
+		
+		this.y_scale = d3.scaleBand()
+						.domain(this.data.map( d => d.run_id))
+						.range([0, this.height])
+						.padding(1.5);
 
 	}
 	
@@ -69,22 +71,18 @@ class sucker_chart {
 						.attr("class", "x axis")
 						.call(d3.axisTop(this.x_scale))
 						.selectAll("text")
-						.attr("transform", "translate(0,0)rotate(0)")
+						.attr("class", "axis_text axis_text_number")
 						.style("text-anchor", "center");
 		
 		this.y_axis = this.svg.append("g")
 						.attr("class", "y axis")
-						.call(d3.axisLeft(this.y_scale2))
+						.call(d3.axisLeft(this.y_scale))
 						.selectAll("text")
+						.attr("class", "axis_text axis_text_runs")
 						.style("text-anchor", "center");
 	}
 
-	create_shapes(col_x, col_y) {
-		const x = col_x;
-		const y = col_y; 
-
-		let svg = this.svg;
-		let data = this.data;
+	create_shapes() {
 
 		this.color = d3.scaleOrdinal()
 			.domain(
@@ -107,43 +105,29 @@ class sucker_chart {
 					"#8b4513", "#f08080",
 					"#80f080", "#ff682a"]);
 
-		var lines = svg.selectAll("lines")
-						.data(data)
+		var lines = this.svg.selectAll("lines")
+						.data(this.data)
 						.enter()
 						.append("line")
-						.attr("x1", (d) => { 
-							return this.x_scale(d[x]); 
-						})
+						.attr("x1", d => this.x_scale(d[this.x]))
 						.attr("x2", this.x_scale(0))
-						.attr("y1", (d) => { 
-							return this.y_scale2(d[y]);
-						})
-						.attr("y2", (d) => {
-							return this.y_scale2(d[y]);
-						})
+						.attr("y1", d => this.y_scale(d.run_id))
+						.attr("y2", d => this.y_scale(d.run_id))
 						.attr("stroke-width", "3")
 						.attr("stroke", "black")
-						.attr("class", function (d) { return "line "+ d.team.toLowerCase(); });
+						.attr("class", d => "line " + d.team.toLowerCase());
 		
-		var circles = svg.selectAll("circles")
-							.data(data)
+		var circles = this.svg.selectAll("circles")
+							.data(this.data)
 							.enter()
 							.append("circle")
-							.attr("cx", (d) => {
-								return this.x_scale(d[x]);
-							})
-							.attr("cy", (d) => {
-								return this.y_scale2(d[y]);
-							})
-							.style("fill", (d) => {
-								return this.color(d.team);
-							})
+							.attr("cx", d => this.x_scale(d[this.x]))
+							.attr("cy", d => this.y_scale(d.run_id))
+							.style("fill", d => this.color(d.team))
 							.attr("r", "10")
-							.attr("stroke-width", "1")
-							.attr("stroke", (d) => {
-								return this.color(d.team);
-							})
-							.attr("class", function (d) { return "circle " + d.team.toLowerCase(); });
+							.attr("stroke-width", "3")
+							.attr("stroke", d => this.color(d.team))
+							.attr("class", d => "circle " + d.team.toLowerCase());
 	}
 
 	set_data(new_data) {
@@ -151,28 +135,25 @@ class sucker_chart {
 		this.draw();
 	}
 
-	sort(column) {
-		let sorted = this.data.sort((b, a) => {
-			return a[column] - b[column];
-		});
+	sort(col) {
+		let sorted;
+		if (this.ascending) {
+			sorted = this.data.sort( (b,a) => b[col] - a[col]);
+		} else {
+			sorted = this.data.sort((b, a) => a[col] - b[col]);
+		}
 		this.set_data(sorted);
 	}
 
 	norm(column) {
-		this.normalize = 1;
-		
-		this.col_x = column;
-
-		this.sort(this.col_x);
+		this.normalize = true;
+		this.x = column;
+		this.sort(this.x);
 	}
 
 	reset(column) {
-		this.normalize = 0;
-		this.col_x = column;
-		this.sort(this.col_x);
-	}
-
-	resize_wins() {
-		this.draw();
+		this.normalize = false;
+		this.x = column;
+		this.sort(this.x);
 	}
 }
--- public/js/wins.js
+++ public/js/wins.js
@@ -21,7 +21,24 @@ d3.csv("public/data/overall_analysis_v2.csv", row_converter, (dataset) => {
 		plot_data: dataset,
 		element: "#win_plot",
 		x: "wins",
-		y: "run_id"
+		filter: 10,
+		ascending: false
+	})
+
+	var podium_analysis = new sucker_chart({
+		plot_data: dataset,
+		element: "#podium_plot",
+		x: "podiums",
+		filter: 10,
+		ascending: false
+	})
+
+	var averages_analysis = new sucker_chart({
+		plot_data: dataset,
+		element: "#average_plot",
+		x: "p_average",
+		filter: 10,
+		ascending: true
 	})
 })
 
