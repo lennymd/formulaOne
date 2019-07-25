28f7f74bb02fb63e168ae52f12f30296bdd4f854
Lenny Martinez <lennymartinezd@gmail.com>
2019-07-24 21:08:50 -0400

    setup podiums

--- public/js/main.js
+++ public/js/main.js
@@ -11,8 +11,14 @@ var scrolly1 = main.select("#scrolly_wins"),
 	steps_wins = scrolly1.select("article")
 						.selectAll(".step");
 
+var scrolly2 = main.select("#scrolly_podiums"),
+	figure_podiums = scrolly2.select("figure"),
+	steps_podiums = scrolly2.select("article")
+							.selectAll(".step");
+
 var scroller_drivers = scrollama(),
-	scroller_wins = scrollama();
+	scroller_wins = scrollama(),
+	scroller_podiums = scrollama();
 
 
 function handleResize() {
@@ -20,19 +26,31 @@ function handleResize() {
 	var h = Math.floor(window.innerHeight * 0.75);
 	steps_bubbles.style('height', h + 'px');
 	steps_wins.style("height", h + "px");
+	steps_podiums.style("height", h + "px");
+
 	var figureWidth= window.innerWidth;
 	var figureMarginTop = 0;
 
 	figure_bubbles
 		.style('width', figureWidth + 'px')
 		.style('top', figureMarginTop + 'px');
+	
+	figure_wins
+		.style('width', figureWidth + 'px')
+		.style('top', figureMarginTop + 'px');
+
+	figure_podiums
+		.style('width', figureWidth + 'px')
+		.style('top', figureMarginTop + 'px');
 
 	scroller_drivers.resize();
+	scroller_wins.resize();
+	scroller_podiums.resize();
 }
 
 function setupStickyfill() {
 	// add stickyfill to all the figures
-	// d3.selectAll('figure').each(() => Stickyfill.add(this));
+	d3.selectAll('.sticky').each(() => Stickyfill.add(this));
 }
 
 function init() {
@@ -54,7 +72,6 @@ function init() {
 		.onStepExit(stepExit_bubble);
 	
 	
-		
 	// load wins stuff
 	d3.csv("public/data/overall_analysis_v2.csv", row_converter, (dataset) => {
 		var win_analysis = new sucker_chart({
@@ -64,8 +81,23 @@ function init() {
 			y: "run_id"
 		})
 
+		var podium_analysis = new sucker_chart({
+			plot_data: dataset,
+			element: "#podium_plot",
+			x: "podiums",
+			y: "run_id"
+		})
+
 		scroller_wins.setup({
 			step:"#scrolly_wins article .step",
+			offset: 0.5,
+			debug: false
+		})
+			.onStepEnter(stepEnter_bubble)
+			.onStepExit(stepExit_bubble);
+
+		scroller_podiums.setup({
+			step:"#scrolly_podiums article .step",
 			offset: 0.5,
 			debug: false
 		})
