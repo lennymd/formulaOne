cb1608340557f01932762b13a214f2038c5cf71c
Lenny Martinez <lennymartinezd@gmail.com>
2019-07-29 22:38:33 -0400

    work on horizontal stack bar chart

--- public/js/bars.js
+++ public/js/bars.js
@@ -1,107 +1,50 @@
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 20, left: 50},
+var margin = {top: 30, right: 10, bottom: 0, left: 30},
 	width = window.innerWidth - margin.left - margin.right,
 	height = window.innerHeight - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
 var svg = d3.select("#stacked")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+			.append("svg")
+			.attr("viewBox", [0, 0, width, height])
+			.append("g")
+			.attr("transform",
+			"translate(" + margin.left + "," + margin.top + ")");
 
-// Parse the Data
-d3.csv("../../public/data/test.csv", function(data) {
+var data = (await d3.csv("https://gist.githubusercontent.com/mbostock/5429c74d6aba68c52c7b39642c98deed/raw/50a5157a1d920191b0a7f636796ee721047cbb92/us-population-state-age.csv", (d, i, columns) => (d3.autoType(d), d.total = d3.sum(columns, c => d[c]), d))).sort((a, b) => b.total - a.total);
 
-  // List of subgroups = header of the csv files = soil condition here
-  var subgroups = data.columns.slice(1)
+var series = d3.stack().keys(data.columns.slice(1))(data);
 
-  // List of groups = species here = value of the first column called group -> I show them on the X axis
-  var groups = d3.map(data, function(d){return(d.group)}).keys()
+var x = d3.scaleLinear()
+			.domain([0, d3.max(series, d => d3.max(d, d => d[1]))])
+			.range([margin.left, width - margin.right]);
 
-  // Add X axis
-  var x = d3.scaleBand()
-      .domain(groups)
-      .range([0, width])
-      .padding([0.2])
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).tickSizeOuter(0));
+var y = d3.scaleBand()
+			.domain(data.map(d => d.name))
+			.range([margin.top, height - margin.bottom])
+			.padding(0.1);
 
-  // Add Y axis
-  var y = d3.scaleLinear()
-    .domain([0, 60])
-    .range([ height, 0 ]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
+var xAxis = g => g.attr("transform", `translate(0,${margin.top})`)
+					.call(d3.axisTop(x).ticks(width / 100, "s"))
+					.call(g => g.selectAll(".domain").remove());
 
-  // color palette = one color per subgroup
-  var color = d3.scaleOrdinal()
-    .domain(subgroups)
-    .range(['#C7EFCF','#FE5F55','#EEF5DB'])
+var yAxis = g => g.attr("transform", `translate(${margin.left},0)`)
+					.call(d3.axisLeft(y).tickSizeOuter(0))
+					.call(g => g.selectAll(".domain").remove());
 
-  //stack the data? --> stack per subgroup
-  var stackedData = d3.stack()
-    .keys(subgroups)
-    (data)
+svg.append("g")
+	.selectAll("g")
+	.data(series)
+	.join("g")
+	.attr("fill", d => color(d.key))
+	.selectAll("rect")
+	.data(d => d)
+	.join("rect")
+	.attr("x", d => x(d[0]))
+	.attr("y", (d, i) => y(d.data.name))
+	.attr("width", d => x(d[1]) - x(d[0]))
+	.attr("height", y.bandwidth());
 
+svg.append("g")
+	.call(xAxis);
 
-
-
-  // ----------------
-  // Create a tooltip
-  // ----------------
-  var tooltip = d3.select("#stacked")
-    .append("div")
-    .style("opacity", 0)
-    .attr("class", "tooltip")
-    .style("background-color", "white")
-    .style("border", "solid")
-    .style("border-width", "1px")
-    .style("border-radius", "5px")
-    .style("padding", "10px")
-
-  // Three function that change the tooltip when user hover / move / leave a cell
-  var mouseover = function(d) {
-    var subgroupName = d3.select(this.parentNode).datum().key;
-    var subgroupValue = d.data[subgroupName];
-    tooltip
-        .html("subgroup: " + subgroupName + "<br>" + "Value: " + subgroupValue)
-        .style("opacity", 1)
-  }
-  var mousemove = function(d) {
-    tooltip
-      .style("left", (d3.mouse(this)[0]+90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
-      .style("top", (d3.mouse(this)[1]) + "px")
-  }
-  var mouseleave = function(d) {
-    tooltip
-      .style("opacity", 0)
-  }
-
-
-
-
-  // Show the bars
-  svg.append("g")
-    .selectAll("g")
-    // Enter in the stack data = loop key per key = group per group
-    .data(stackedData)
-    .enter().append("g")
-      .attr("fill", function(d) { return color(d.key); })
-      .selectAll("rect")
-      // enter a second time = loop subgroup per subgroup to add all rectangles
-      .data(function(d) { return d; })
-      .enter().append("rect")
-        .attr("x", function(d) { return x(d.data.group); })
-        .attr("y", function(d) { return y(d[1]); })
-        .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-        .attr("width",x.bandwidth())
-        .attr("stroke", "grey")
-      .on("mouseover", mouseover)
-      .on("mousemove", mousemove)
-      .on("mouseleave", mouseleave)
-
-})
+svg.append("g")
+	.call(yAxis);
