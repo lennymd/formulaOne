e2b8743f02bb3a12017a229bc98ab3fb58b6ce0c
Lenny Martinez <lennymartinezd@gmail.com>
2019-08-01 14:10:45 -0400

    clean main js file

--- public/js/main.js
+++ public/js/main.js
@@ -1,63 +1,27 @@
 // initialize the big variables
 var main = d3.select("main");
 
-// var scrolly0 = main.select("#scrolly_bubbles"),
-// 	figure_bubbles = scrolly0.select("figure"),
-// 	steps_bubbles = scrolly0.select("article")
-// 							.selectAll(".step");
-
 var scrolly1 = main.select("#scrolly_wins"),
 	figure_wins = scrolly1.select("figure"),
-	steps_wins = scrolly1.select("article")
-						.selectAll(".step");
+	steps_wins = scrolly1.select("article").selectAll(".step");
 
-var scrolly2 = main.select("#scrolly_podiums"),
-	figure_podiums = scrolly2.select("figure"),
-	steps_podiums = scrolly2.select("article")
-							.selectAll(".step");
 
-var scrolly3 = main.select("#scrolly_averages"),
-	figure_averages = scrolly3.select("figure"),
-	steps_averages = scrolly3.select("article")
-							.selectAll(".step");
-
-var scroller_drivers = scrollama(),
-	scroller_wins = scrollama(),
-	scroller_podiums = scrollama(),
-	scroller_averages = scrollama();
+var scroller_wins = scrollama();
 
 
 function handleResize() {
 	// this should handle window size changing
-	var h = Math.floor(window.innerHeight / 0.5);
-	// steps_bubbles.style('min-height', h + 'px');
-	steps_wins.style("min-height", h + "px");
-	steps_podiums.style("min-height", h + "px");
-	steps_averages.style("min-height", h + "px");
+	var h = Math.floor(window.innerHeight * 0.75);
+	steps_wins.style("height", h + "px");
 
 	var figureHeight= window.innerHeight / 2;
 	var figureMarginTop = (window.innerHeight - figureHeight) / 2;
-
-	// figure_bubbles
-	// 	.style('width', figureWidth + 'px')
-	// 	.style('top', figureMarginTop + 'px');
 	
 	figure_wins
 		.style('width', figureHeight + 'px')
 		.style('top', figureMarginTop + 'px');
 
-	figure_podiums
-		.style('width', figureHeight + 'px')
-		.style('top', figureMarginTop + 'px');
-	
-	figure_averages
-		.style('width', figureHeight + 'px')
-		.style('top', figureMarginTop + 'px');
-
-	// scroller_drivers.resize();
 	scroller_wins.resize();
-	scroller_podiums.resize();
-	scroller_averages.resize();
 }
 
 function setupStickyfill() {
@@ -74,16 +38,6 @@ function init() {
 	// force a resize to make sure things are most up to date
 	handleResize();
 
-	// work with bubble chart
-	// scroller_drivers.setup({
-	// 	step: '#scrolly_bubbles article .step',
-	// 	offset: 0.5,
-	// 	debug: false
-	// })
-	// 	.onStepEnter(stepEnter_bubble)
-	// 	.onStepExit(stepExit_bubble);
-	
-	
 	// load wins stuff
 	d3.csv("public/data/overall_analysis_v2.csv", row_converter, (dataset) => {
 		var win_analysis = new sucker_chart({
@@ -92,22 +46,6 @@ function init() {
 			x: "wins",
 			filter: 10
 		})
-
-		// var podium_analysis = new sucker_chart({
-		// 	plot_data: dataset,
-		// 	element: "#podium_plot",
-		// 	x: "podiums",
-		// 	filter: 10,
-		// 	ascending: false
-		// })
-
-		// var averages_analysis = new sucker_chart({
-		// 	plot_data: dataset,
-		// 	element: "#averages_plot",
-		// 	x: "p_average",
-		// 	filter: 10,
-		// 	ascending: true
-		// })
 
 		function stepEnter(response) {
 			const element = d3.select(response.element);
@@ -126,7 +64,7 @@ function init() {
 					console.log(section, index, "enter");
 					if (index === 1) {
 						//update to be about win percentages
-						podium_analysis.update("podium_percentage", true, 10);
+						// podium_analysis.update("podium_percentage", true, 10);
 					}
 				} else {
 					// section ==== 3
@@ -148,7 +86,7 @@ function init() {
 					console.log(section, index, "enter");
 					if (index === 0) {
 						//update to be about win percentages
-						podium_analysis.update("podiums", false, 11);
+						// podium_analysis.update("podiums", false, 11);
 					}
 				} else {
 					// section ==== 3
@@ -195,23 +133,6 @@ function init() {
 		})
 			.onStepEnter(stepEnter)
 			.onStepExit(stepExit);
-
-		// scroller_podiums.setup({
-		// 	step:"#scrolly_podiums article .step",
-		// 	offset: 0.5,
-		// 	debug: false
-		// })
-		// .onStepEnter(stepEnter)
-		// .onStepExit(stepExit);
-		
-		// scroller_averages.setup({
-		// 		step:"#scrolly_averages article .step",
-		// 		offset: 0.5,
-		// 		debug: false
-		// 	})
-		// 	.onStepEnter(stepEnter_bubble)
-		// 	.onStepExit(stepExit_bubble);
-
 	})
 
 
