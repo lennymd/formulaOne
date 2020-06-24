9244a3af0e4755cec34b4b3fa0a2f9d53a2da03f
Lenny Martinez <lennymartinezd@gmail.com>
2020-06-24 11:04:02 -0400

    finish script for scraping race list

--- _analysis/scripts/get_races_statsf1.py
+++ _analysis/scripts/get_races_statsf1.py
@@ -1,16 +1,40 @@
 import requests
-import csv
 from bs4 import BeautifulSoup
-from time import sleep
+from time import sleep, process_time
+import csv
 
 race_list = [["year", "round", "name", "url"]]
+headers = {"User-Agent": "test"}
+BASE_URL = "https://www.statsf1.com"
 
-BASE_URL = "https://www.statsf1.com/en/"
+for year in range(1950, 2020):
+    # add the url for the page
+    t_start = process_time()
+    ending = "/en/" + str(year) + ".aspx"
+    response = requests.get(BASE_URL + ending, headers=headers)
 
-# for year in range(1950, 2020):
-year = 2019
-ending = str(year) + ".aspx"
-print(BASE_URL + ending)
-response = requests.get(BASE_URL + ending)
+    # creat BeautifulSoup object that will be parsed
+    soup = BeautifulSoup(response.text, "lxml")
 
-print(response.text)
+    # find all the races
+    races = soup.find("div", class_="yeargp").find_all("div", class_="gp")
+
+    # extract info from each race
+    for race in races:
+        prix_info = race.find("div", class_="pays").text.strip()
+
+        (round_id, name) = prix_info.split(". ")
+
+        rel_url = race.find("div", class_="flag").find("a").attrs["href"]
+
+        race_list.append([year, round_id, name, rel_url])
+    t_end = process_time()
+    print("Time elapsed for", year, ":", t_end - t_start)
+
+    # add some time buffer every 10 years
+    if (year % 10) == 0:
+        print("Pause ", year)
+        sleep(1)
+
+with open("../data/race_list_statsf1.csv", "w") as output:
+    csv.writer(output, delimiter=",").writerows(race_list)
